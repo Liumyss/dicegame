e8c52c8e16ddbc641365406a60cc74a5c38c7286
liumyss <computer.46kmp@simplelogin.fr>
2022-05-03 12:59:58 -0600

    inital commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dicegame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
